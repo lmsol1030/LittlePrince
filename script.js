8b85ae14984b038052c55daa7d150cdd801fed90
lectureixdl <lecture@ixdl.kr>
2014-05-16 15:55:35 +0900

    Add shake animation

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
 	$("#cloud").click(function(){
 		$(this).css({opacity: "+=0.1"});
 	});
+	$(".item").click(function() {
+		$(this).addClass('shake');
+		var self = this;
+		setTimeout(function(){$(self).removeClass('shake');}, 500);
+	});
 });
