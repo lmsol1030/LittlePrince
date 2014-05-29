29f4b89e420911991df2744cba08c0537a003823
lmsol1030 <lmsol1030@naver.com>
2014-05-29 18:45:00 +0900

    Add many things

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 $(document).ready(function() {
+	$(".page").hide();
+	$("#page-2").show();
+
+	$("#day").click(function(){
+		$("#night").fadeIn(3000);
+	});
+	$("#night").click(function(){
+		$("#night").fadeOut(3000);
+	});
+	$("#page-2 .cloud").animateSprite({
+	    fps: 3,
+	    complete: function(){
+	        // use complete only when you set animations with 'loop: false'
+    	}
+	});
+	$("#page-2 .cloud").animateSprite('stop');
+	$("#page-2 .cloud").click(function(){
+		$("#page-2 .cloud").animateSprite('restart');
+	});
+});
+
+/*
+$(document).ready(function() {
 	$("#cloud").click(function(){
 		$(this).css({opacity: "+=0.1"});
 	});
@@ -7,4 +30,8 @@ $(document).ready(function() {
 		var self = this;
 		setTimeout(function(){$(self).removeClass('shake');}, 500);
 	});
+
+
+	
 });
+*/
