a53f9a7c8cad2d3cd3ed4dbd1e6d0a4b94829d30
kangdongkil <kbk3418@naver.com>
2014-06-21 15:47:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 		$("#night").fadeOut(3000);
 	});
 	$("#page-2 .cloud").animateSprite({
-	    fps: 3,
+	    fps: 7,
 	    complete: function(){
 	        // use complete only when you set animations with 'loop: false'
     	}
