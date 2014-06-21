bc8a8a46fb7e3f1425527d209f0da87110f4f277
kangdongkil <kbk3418@naver.com>
2014-06-21 18:55:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
 	$(".page").hide();
 	$("#page-2").show();
@@ -11,20 +10,21 @@ $(document).ready(function() {
 	$("#page-2 .airplane").click(function(){	
 		$("#page-2 .cloud").show();
 		$("#page-2 .cloud").animateSprite({
-				fps:7,
-				loop: true,
+			fps:7,
+			loop: true,
 		});
 	});
 	$("#page-2 .typo").click(function(){
 		$("#page-2 .text").show();
 		$("#page-2 .text").typed({
-        	strings: ["넓은 바다 한가운데서 혼자 뗏목을 타고 표류하는 것보다 더 고립되어 있는 것처럼 느껴졌다!"],
-        	typeSpeed: 100,
-        	callback: function(){
-		$("#page-2").hide();
-		$("#page-3").show();
-		});
-      		
+			strings: ["넓은 바다 한가운데서 혼자 뗏목을 타고 표류하는 것보다 더 고립되어 있는 것처럼 느껴졌다!"],
+			typeSpeed: 100,
+			callback: function(){
+				$("#page-2").hide();
+				$("#page-3").show();
+				}
+			});
+
 	});
 });
 
