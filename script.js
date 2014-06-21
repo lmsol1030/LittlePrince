97bd0b211317a8e8b9818fbd749e0f2e89e04c33
kangdongkil <kbk3418@naver.com>
2014-06-22 00:46:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -75,6 +75,14 @@ $(document).ready(function(){
 $(document).ready(function(){
 	$(".page").hide();
 	$('#page-4').show();
+	$("#page-4 .typo").click(function(){
+		$("#page-4 .text").show();
+		$("#page-4 .text").typed({
+			strings: ["넓은 바다 한가운데서 혼자 뗏목을 타고 표류하는 것보다 더 고립되어 있는 것처럼 느껴졌다!"],
+			typeSpeed: 130,				}
+			});
+
+	});
 	
 });
 
