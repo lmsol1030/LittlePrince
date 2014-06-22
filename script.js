4ed2ad0a22bb70c1503d128af975414b065296bd
kangdongkil <kbk3418@naver.com>
2014-06-22 19:02:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -57,6 +57,12 @@ $(document).ready(function() {
 			});
 
 		});
+		google_ad_client = "ca-pub-2783044520727903";
+		/* jQuery_demo */
+		google_ad_slot = "2780937993";
+		google_ad_width = 728;
+		google_ad_height = 90;
+		//-->
 		  var _gaq = _gaq || [];
   		_gaq.push(['_setAccount', 'UA-36251023-1']);
 		 _gaq.push(['_setDomainName', 'jqueryscript.net']);
